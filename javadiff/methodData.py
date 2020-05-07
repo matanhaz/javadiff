e221cc6a773449e97777760e230cc8dd698eaf16
Amir Elmishali <amir9979@gmail.com>
2020-05-07 03:14:51 +0300

    Update methodData.py

--- javadiff/methodData.py
+++ javadiff/methodData.py
@@ -1,10 +1,10 @@
 import operator
 import javalang
-
+from collections import Counter
 
 class SourceLine(object):
     def __init__(self, line, line_number, is_changed, ordinal, decls):
-        self.line = line
+        self.line = line.strip()
         self.line_number = line_number
         self.is_changed = is_changed
         self.ordinal = ordinal
@@ -22,7 +22,7 @@ class SourceLine(object):
         for line_number in range(start_line - 1, end_line):
             if line_number not in method_used_lines:
                 continue
-            decls = SourceLine.get_decl_at_line(parsed_body, line_number)
+            decls = SourceLine.get_decl_at_line(parsed_body, line_number+1)
             line = contents[line_number]
             is_changed = line_number in changed_indices
             source_lines.append(SourceLine(line, line_number, is_changed, line_number-start_line, decls))
@@ -32,8 +32,10 @@ class SourceLine(object):
     def get_decl_at_line(parsed_body, line_number):
         ans = []
         for e in parsed_body:
-            ans.extend(list(filter(lambda x: x.position and x.position.line==line_number, map(operator.itemgetter(1), e.filter(javalang.ast.Node)))))
-        return set(map(lambda x: type(x).__name__, ans))
+            for e2 in map(operator.itemgetter(1), e.filter(object)):
+                if any(list(filter(lambda x: x.position and x.position.line==line_number, map(operator.itemgetter(1), e2.filter(javalang.ast.Node))))):
+                    ans.append(e2)
+        return dict(Counter(map(lambda x: type(x).__name__, ans)))
 
 
 class MethodData(object):
